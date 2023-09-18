55ef48155334b197e49a4064c2ddb87541abf716
10612 <yoavr123@gamil.com>
2023-09-19 00:58:24 +0300

    Added board

---
+++ src/components/board/board.tsx
@@ -0,0 +1,18 @@
+import { Paper } from "@mui/material";
+import { useState } from "react";
+import { makeStartingBoard } from "./utils";
+import Tile from "../tile";
+import { board } from "./styles";
+
+export default function Board(): JSX.Element {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [boardValues, setBoardValues] = useState(makeStartingBoard());
+
+	return (
+		<Paper sx={board}>
+			{boardValues.map((value) => (
+				<Tile value={value} />
+			))}
+		</Paper>
+	);
+}

69cccb4ee3d7e7a2de50338f78621fb33f1c2234
10612 <yoavr123@gamil.com>
2023-09-19 00:49:24 +0300

    Added tiles

---
+++ src/components/tile/styles.ts
@@ -0,0 +1,57 @@
+import { SystemStyleObject } from "@mui/system";
+
+export const tileStyles: Record<number, SystemStyleObject> = {
+	0: {
+		backgroundColor: "#cdc1b4",
+	},
+	2: {
+		color: "#776e65",
+		backgroundColor: "#eee4da",
+	},
+	4: {
+		color: "#776e65",
+		backgroundColor: "#eee1c9",
+	},
+	8: {
+		backgroundColor: "#f3b27a",
+	},
+	16: {
+		backgroundColor: "#f69664",
+	},
+	32: {
+		backgroundColor: "#f77c5f",
+	},
+	64: {
+		backgroundColor: "#f75f3b",
+	},
+	128: {
+		backgroundColor: "#edd073",
+		boxShadow: 1,
+		fontSize: "45px",
+	},
+	256: {
+		backgroundColor: "#edcc62",
+		boxShadow: 2,
+		fontSize: "45px",
+	},
+	512: {
+		backgroundColor: "#edc950",
+		boxShadow: 3,
+		fontSize: "45px",
+	},
+	1024: {
+		backgroundColor: "#edc53f",
+		boxShadow: 4,
+		fontSize: "35px",
+	},
+	2048: {
+		backgroundColor: "#edc22e",
+		boxShadow: 5,
+		fontSize: "35px",
+	},
+};
+
+export const over2048 = {
+	backgroundColor: "#3c3a33",
+	fontSize: "30px",
+};

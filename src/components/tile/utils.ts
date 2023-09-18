83d7758d2dc67e5ebfd960456d4376666cbd61d4
10612 <yoavr123@gmail.com>
2023-09-19 01:24:30 +0300

    Revert "Create board"

--- src/components/tile/utils.ts
+++
@@ -1,6 +0,0 @@
-import { SystemStyleObject } from "@mui/system";
-import { over2048, tileStyles } from "./styles";
-
-export function numberStyle(value: number): SystemStyleObject {
-	return value <= 2048 ? tileStyles[value] : over2048;
-}
